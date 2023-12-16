4257d79f4c97de4d8ee4339f277b48e7e9255417
Zacarías <zacarias@wichipu.com>
2023-12-16 08:55:38 +0100

    refactor: Changed the CommandHandler contract.
    To make the type of the input method arguments less restrictive.

--- src/Application/Command.php
+++ src/Application/Command.php
@@ -6,5 +6,4 @@ namespace App\Application;
 
 interface Command
 {
-
 }
--- src/Application/CommandHandler.php
+++ src/Application/CommandHandler.php
@@ -6,5 +6,4 @@ namespace App\Application;
 
 interface CommandHandler
 {
-    public function __invoke(Command $command): void;
 }
