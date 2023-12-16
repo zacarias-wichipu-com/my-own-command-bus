373dfb908ce9a9bf12613b42dfa229127a560479
Zacarías <zacarias@wichipu.com>
2023-12-16 08:18:45 +0100

    feat: Added initial log implementation.
    Added next to the core command bus behaviour.

--- src/CommandBus.php
+++ src/CommandBus.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace App;
 
 use App\Application\Command;
+use DateTimeImmutable;
 
 final readonly class CommandBus
 {
@@ -15,7 +16,28 @@ final readonly class CommandBus
 
     public function __invoke(Command $command): void
     {
+        $log = fopen(
+            filename: dirname(__DIR__) . '/var/clock.log',
+            mode: 'a'
+        );
+        fwrite(
+            stream: $log,
+            data: sprintf(
+                '%1$s: Executing %2$s' . PHP_EOL,
+                (new DateTimeImmutable())->format('Y-m-d H:i:s'),
+                $command::class
+            )
+        );
         $handler = $this->commandHandlerDriver->getCommandHandlerFor(command: $command);
         ($handler)(command: $command);
+        fwrite(
+            stream: $log,
+            data: sprintf(
+                '%1$s: %2$s finished' . PHP_EOL,
+                (new DateTimeImmutable())->format('Y-m-d H:i:s'),
+                $command::class
+            )
+        );
+        fclose($log);
     }
 }
